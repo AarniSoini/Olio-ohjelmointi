44f5e7313261e3d78c7d23772ffac64f40c53806
Aarni <115700330+AarniSoini@users.noreply.github.com>
2023-02-06 11:52:31 +0200

    lol

---
+++ Arvauspeli.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int arvaus;
+        int vastaus;
+        while True()
+        {
+            Console.WriteLine("Arvaa luku 1-10");
+            arvaus = Console.ReadLine();
+        }
+    }
+}
